c48f4aaa0da7689883517e0e1d484aa66d7b827b
tonyliu0910 <lyt1658@gmail.com>
2023-09-26 18:42:32 +0800

    update

--- experiment.sh
+++ experiment.sh
@@ -1,2 +1,2 @@
 accelerate launch DiffuEMG/main.py --data_cfg data_cfg.yaml --train
-python DiffuEMG/main.py --data_cfg data_cfg.yaml --test --sample --test_mismatch
+python DiffuEMG/main.py --data_cfg data_cfg.yaml --test --sample
